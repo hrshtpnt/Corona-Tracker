7801ce28ad98440e472049861206b4fed758b9b7
Harshit Pant <hapant@deloitte.com>
2020-05-26 17:02:14 +0530

    updated css

---
+++ precache-manifest.27d37341bdbe35695f9f63e67846c926.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84d2e1c5a7503230be432bde90343234",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8daae565de1b0aed4ef9",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "6209e8d16ca3638ab0a0",
+    "url": "/static/css/main.7a509aea.chunk.css"
+  },
+  {
+    "revision": "8daae565de1b0aed4ef9",
+    "url": "/static/js/2.424405c4.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/static/js/2.424405c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6209e8d16ca3638ab0a0",
+    "url": "/static/js/main.c7a48939.chunk.js"
+  },
+  {
+    "revision": "84599ef62b7876584165",
+    "url": "/static/js/runtime-main.5948f688.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/corona.d7265326.png"
+  },
+  {
+    "revision": "f94dfe67b20b9d07c6d11e4d79bb5d7c",
+    "url": "/static/media/sort-black.f94dfe67.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Corona-Tracker/precache-manifest.772c66e006904d54d0acad11e8d49a17.js"
+  "/precache-manifest.27d37341bdbe35695f9f63e67846c926.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Corona-Tracker/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
